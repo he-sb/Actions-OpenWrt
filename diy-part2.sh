eb149774c9ed0a43d167136a0c50b980cd46db88
he-sb <0he.sb0@gmail.com>
2022-02-27 10:21:53 +0800

    Added zsh related configs

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,15 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.3/g' package/base-files/files/bin/config_generate
+
+# Change default shell to zsh
+sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
+
+## Install oh-my-zsh
+# Clone oh-my-zsh repository
+git clone https://github.com/robbyrussell/oh-my-zsh ./files/root/.oh-my-zsh
+
+# Install extra plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ./files/root/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ./files/root/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-completions ./files/root/.oh-my-zsh/custom/plugins/zsh-completions
